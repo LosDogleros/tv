a5b066c1b248df5cca5ed80e0b89d38186993174
LosDogleros <71036913+LosDogleros@users.noreply.github.com>
2021-01-05 18:32:43 -0600

    Add files via upload

---
+++ js/locked.js
@@ -0,0 +1,13 @@
+document.oncontextmenu = function(){return false}
+  function disableselect(e){
+    return false
+      }
+        function reEnable(){
+          return true
+            }
+
+document.onselectstart=new Function ("return false")
+  if (window.sidebar){
+    document.onmousedown=disableselect
+      document.onclick=reEnable
+        }
